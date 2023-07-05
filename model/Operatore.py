4da28c6f1796221a30336ee3c2a93d5285508555
LoriGaetani <100944852+LoriGaetani@users.noreply.github.com>
2023-07-05 12:18:59 +0200

    +- CRUD operatore

--- model/Operatore.py
+++ model/Operatore.py
@@ -8,8 +8,35 @@ class Operatore:
         self.email = email
         self.telefono = telefono
 
+    def get_codice_fiscale(self):
+        return self.codiceFiscale
+
+    def set_nome(self, nuovo_nome):
+        self.nome = nuovo_nome
+
+    def get_nome(self):
+        return self.nome
+
+    def set_cognome(self, nuovo_cognome):
+        self.cognome = nuovo_cognome
+
+    def get_cognome(self):
+        return self.cognome
+
+    def set_email(self, nuova_email):
+        self.email = nuova_email
+
+    def get_email(self):
+        return self.email
+
+    def set_telefono(self, nuovo_telefono):
+        self.telefono = nuovo_telefono
+
+    def get_telefono(self):
+        return self.telefono
+
     def stampa(self):
-        print(self.codiceFiscale+self.nome+self.cognome+self.email+self.telefono)
+        print(self.codiceFiscale+' '+self.nome+' '+self.cognome+' '+self.email+' '+self.telefono)
 
     def inserisci_operatore(data):
         with open('Operatori.pickle', 'ab') as file:
@@ -18,18 +45,64 @@ class Operatore:
 
 
 
-    def ricerca_operatore(operatore):
-        with open('Operatori.pickle', 'rb') as f:
-            try:
+    def ricerca_operatore(cf):
+        try:
+            with open('Operatori.pickle', 'rb') as f:
                 while True:
-                    data = pickle.load(f)
-
-                    if (operatore['codiceFiscale'] == data['codiceFiscale']):
-                        return data
-            except EOFError:
-                pass
+                    try:
+                        data = pickle.load(f)
+                        if data.get_codice_fiscale() == cf:
+                            return data
+                    except EOFError:
+                        break
+        except FileNotFoundError:
+            print("Il file specificato non esiste.")
+        except Exception as e:
+            print(f"Si è verificato un errore: {str(e)}")
 
         return None
+
+    def modifica_operatore(operatore, new_data):
+        with open('Operatori.pickle', 'rb') as f:
+            objects = []
+            while True:
+                try:
+                    obj = pickle.load(f)
+                    if obj.get_codice_fiscale() == operatore.get_codice_fiscale():
+                        obj.set_nome(new_data['nome'])
+                        obj.set_cognome(new_data['cognome'])
+                        obj.set_email(new_data['email'])
+                        obj.set_telefono(new_data['telefono'])
+                    objects.append(obj)
+                except EOFError:
+                    break
+
+        with open('Operatori.pickle', 'wb') as f:
+            for obj in objects:
+                pickle.dump(obj, f)
+
+    def elimina_operatore(operatore):
+        with open('Operatori.pickle', 'rb') as f:
+            objects = []
+            while True:
+                try:
+                    obj = pickle.load(f)
+                    if obj.get_codice_fiscale() == operatore.codiceFiscale:
+                        pass
+                    else:
+                        objects.append(obj)
+                except EOFError:
+                    break
+
+        with open('Operatori.pickle', 'wb') as f:
+            for obj in objects:
+                pickle.dump(obj, f)
+
+
+
+
+
+
 
 
 
--- view/gestioneOperatore.py
+++ view/gestioneOperatore.py
@@ -1,13 +1,17 @@
 import PySimpleGUI as sg
 from model import Operatore
 from controller import gestioneOperatore
+import pickle
+
+
+
 
 view_gestione_operatore = [[sg.Button('Inserisci un nuovo operatore')],
                      [sg.Button('Visualizza un operatore')],
                      [sg.Button('Modifica un operatore')],
                      [sg.Button('Elimina un operatore')]]
 
-view_ricerca_operatore = [[sg.Text('Codice fiscale'), sg.Input(key='codiceFiscale')],[sg.Button('Invia')]]
+view_ricerca_operatore = [[sg.Text('Codice fiscale'), sg.Input(key='codiceFiscale')],[sg.Button('Cerca')]]
 
 
 window_gestione_operatore = sg.Window('Gestione operatore', view_gestione_operatore)
@@ -30,29 +34,96 @@ while True:
                 break
             elif event2 == 'Invia':
                 Operatore.Operatore.inserisci_operatore(values2)
-
-        window_inserisci_operatore.close()
+                window_inserisci_operatore.close()
 
     elif event == 'Visualizza un operatore':
         window_ricerca_operatore = sg.Window('Ricerca operatore', view_ricerca_operatore)
         while True:
             event3, values3 = window_ricerca_operatore.read()
-            if event3 == sg.WINDOW_CLOSED or event3 == "Chiudi":
+            if event3 == sg.WINDOW_CLOSED:
                 break
-            elif event3 == 'Invia':
-                operatore = Operatore.Operatore.ricerca_operatore(values3)
+            elif event3 == 'Cerca':
+                operatore = Operatore.Operatore.ricerca_operatore(str(values3['codiceFiscale']))
+                if operatore:
+                    view_visualizza_operatore = [[sg.Text('Codice fiscale: '),sg.Text(operatore.get_codice_fiscale())],
+                                                 [sg.Text('Nome: '),sg.Text(operatore.get_nome())],
+                                                 [sg.Text('Cognome: '),sg.Text(operatore.get_cognome())],
+                                                 [sg.Text('Email: '),sg.Text(operatore.get_email())],
+                                                 [sg.Text('Telefono: '),sg.Text(operatore.get_telefono())]]
 
-                view_visualizza_operatore = [[sg.Text(operatore['codiceFiscale'])],
-                                             [sg.Text(operatore['nome'])],
-                                             [sg.Text(operatore['cognome'])],
-                                             [sg.Text(operatore['email'])],
-                                             [sg.Text(operatore['telefono'])]]
+                else:
+                    view_visualizza_operatore = [[sg.Text('Operatore non trovato')]]
                 window_visualizza_operatore = sg.Window('Visualizza operatore', view_visualizza_operatore)
+                window_ricerca_operatore.close()
+                while True:
+                    event5, values5 = window_visualizza_operatore.read()
+                    if event5 == sg.WINDOW_CLOSED:
+                        break
+
+
+
+    elif event == 'Modifica un operatore':
+        window_ricerca_operatore = sg.Window('Ricerca operatore', view_ricerca_operatore)
+
+        while True:
+            event4, values4 = window_ricerca_operatore.read()
+            if event4 == sg.WINDOW_CLOSED or event4 == "Chiudi":
+                break
+            elif event4=='Cerca':
+                operatore = Operatore.Operatore.ricerca_operatore(str(values4['codiceFiscale']))
+                if operatore:
+                    modificaOperatore = [
+                        [sg.Text('Nome'), sg.Input(default_text=operatore.get_nome(),key='nome')],
+                        [sg.Text('Cognome'), sg.Input(default_text=operatore.get_cognome(),key='cognome')],
+                        [sg.Text('email'), sg.Input(default_text=operatore.get_email(),key='email')],
+                        [sg.Text('Telefono'), sg.Input(default_text=operatore.get_telefono(),key='telefono')],
+                        [sg.Button('Invia')]]
+                else:
+                    modificaOperatore = [[sg.Text('Operatore non trovato')]]
+                window_modifica_operatore = sg.Window('Modifica operatore', modificaOperatore)
+                window_ricerca_operatore.close()
+                while True:
+                    event6, values6 = window_modifica_operatore.read()
+                    if event6 == sg.WINDOW_CLOSED:
+                        break
+                    elif event6 == 'Invia':
+                        Operatore.Operatore.modifica_operatore(operatore,values6)
+                        window_modifica_operatore.close()
+
+
+
+
+    elif event == 'Elimina un operatore':
+        window_ricerca_operatore = sg.Window('Ricerca operatore', view_ricerca_operatore)
+
+        while True:
+            event7, values7 = window_ricerca_operatore.read()
+            if event7 == sg.WINDOW_CLOSED or event7 == "Chiudi":
+                break
+            elif event7 == 'Cerca':
+                operatore = Operatore.Operatore.ricerca_operatore(str(values7['codiceFiscale']))
+                window_ricerca_operatore.close()
+                if operatore:
+                    Operatore.Operatore.elimina_operatore(operatore)
+                    view_operatore_eliminato = [[sg.Text('Operatore eliminato')]]
+                else:
+                    view_operatore_eliminato = [[sg.Text('Operatore non trovato')]]
+                window_elimina_operatore = sg.Window('Elimina operatore',view_operatore_eliminato)
+                while True:
+                    event8, values8 = window_elimina_operatore.read()
+                    if event8 == sg.WINDOW_CLOSED:
+                        break
+
 
 
 
 
 window_gestione_operatore.close()
+
+
+
+
+
 
 
 
