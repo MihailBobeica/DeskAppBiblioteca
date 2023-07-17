9f05818d5030852f33c061200df45068e180af0c
Mihail Bobeica <mihailbobeica.07@gmail.com>
2023-07-17 15:28:46 +0200

    rollback main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import PySimpleGUI as sg
+import sys
 
-layout = [  [sg.Text('Name'), sg.Input(key='-NAME-')],
-            [sg.Text('Address'), sg.Input(key='-ADDRESS-')],
-            [sg.Text('City and State'), sg.Input(key='-CITY AND STATE-')],
-            [sg.Ok(), sg.Cancel()]]
+from PySide6.QtWidgets import QApplication
 
-window = sg.Window('Simple Inputs', layout, element_justification='r')
+from database import Session
 
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
-        break
 
-window.close()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    db_session = Session()
+
+    from app import main_window
+    main_window.show()
+
+    sys.exit(app.exec())
