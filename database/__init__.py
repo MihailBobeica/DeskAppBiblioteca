a63bcc0d35e1decf5bd826ee273d8025132b5c99
Mina_1999 <96628549+Mina_1999@users.noreply.github.com>
2023-07-18 11:11:01 +0200

    prova

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum, DateTime, Boolean
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
@@ -36,6 +36,19 @@ class Libro(Base):
     isbn = Column(String)
 
 
+class PrenotazionePosto(Base):
+    __tablename__ = 'prenotazioni_posti'
+
+    id = Column(Integer, primary_key=True)
+    data_prenotazione = Column(DateTime)
+    data_effettuazione = Column(DateTime)
+    ora_inizio = Column(String)
+    ora_fine = Column(String)
+    ora_attivazione = Column(String)
+    disponibilita = Column(Boolean)
+    codice_posto = Column(String)
+
+
 utenti = [{"nome": "Mario",
            "cognome": "Rossi",
            "ruolo": "admin",
@@ -53,8 +66,7 @@ utenti = [{"nome": "Mario",
            "password": hash_password("utente")},
           ]
 
-
-Base.metadata.drop_all(db_engine)    # cancella tutte le tabelle
+Base.metadata.drop_all(db_engine)  # cancella tutte le tabelle
 Base.metadata.create_all(db_engine)  # crea tutte le tabelle
 
 # seeding del database
@@ -68,3 +80,26 @@ for u in utenti:
     db_session.add(utente)
 db_session.commit()
 
+#  creare le tabelle nel database utilizzando la classe PrenotazionePosto
+Base.metadata.create_all(db_engine)
+
+# EsempioOoooOO di utilizzo di PrenotazionePosto
+prenotazione = PrenotazionePosto(
+    data_prenotazione="2023-07-18",
+    data_effettuazione="2023-07-19",
+    ora_inizio="09:00",
+    ora_fine="12:00",
+    ora_attivazione="08:45",
+    disponibilita=True,
+    codice_posto="A101"
+)
+
+# inserire la prenotazione nel database
+db_session = Session()
+db_session.add(prenotazione)
+db_session.commit()
+
+# ottenere tutte le prenotazioni dal database
+prenotazioni = db_session.query(PrenotazionePosto).all()
+for prenotazione in prenotazioni:
+    prenotazione.stampa()
