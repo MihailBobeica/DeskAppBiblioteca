586752b34bf55583fa06ceefc8afe00e2a10ed72
Mihail Bobeica <mihailbobeica.07@gmail.com>
2023-07-28 11:57:31 +0200

    merged cahnges in home_utente

--- view/home_utente.py
+++ view/home_utente.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QHBoxLayout
 from abstract.view import View
 from view.component import SidebarComponent
 from view.component.catalogo import CatalogoComponent
+from view.scegli_prenotazione import ScegliPrenotazione
 
 
 class HomeUtenteView(View):
@@ -12,6 +13,7 @@ class HomeUtenteView(View):
                                     "Libri in prestito",
                                     "Libri prenotati",
                                     "Lista di osservazione",
+                                    "Prenota posto",
                                     "Sanzioni",
                                     "Info",
                                     "Logout"),
@@ -26,10 +28,15 @@ class HomeUtenteView(View):
     def connect_buttons(self):
         logout_button = self.get_button("Logout")
         logout_button.clicked.connect(self.send_logout_request)
+        self.get_button("Prenota posto").clicked.connect(self.show_prenota_schermata)
 
     def attach_controllers(self) -> None:
         from app import controller_logout
         self.attach(controller_logout)
+
+    def show_prenota_schermata(self):
+        scegli_prenotazione_view = ScegliPrenotazione()
+        self.main_window.set_view(scegli_prenotazione_view)
 
     def __init__(self):
         super().__init__()
