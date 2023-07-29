c89a5cccac83eaecfc9882cbd04343447285a0f3
Mina_1999 <96628549+Mina_1999@users.noreply.github.com>
2023-07-29 15:45:26 +0200

    lista Prenotazioni

---
+++ controller/gestione_prenotazione_posto.py
@@ -0,0 +1,44 @@
+from abstract.controller import Controller
+from database import PrenotazioneAula, PrenotazionePosto, Session
+
+class PrenotazioneController(Controller):
+    def crea_prenotazione_aula(self, aula, data, utente_id):
+        db_session = Session()
+        prenotazione_aula = PrenotazioneAula(aula=aula, data=data, utente_id=utente_id)
+        db_session.add(prenotazione_aula)
+        db_session.commit()
+        db_session.close()
+
+    def crea_prenotazione_posto(self, posto, data, utente_id):
+        db_session = Session()
+        prenotazione_posto = PrenotazionePosto(posto=posto, data=data, utente_id=utente_id)
+        db_session.add(prenotazione_posto)
+        db_session.commit()
+        db_session.close()
+
+    def get_prenotazioni_aula(self, n):
+        db_session = Session()
+        prenotazioni_aula = PrenotazioneAula().get(n)
+        db_session.close()
+        return prenotazioni_aula
+
+    def get_prenotazioni_posto(self, n):
+        db_session = Session()
+        prenotazioni_posto = PrenotazionePosto().get(n)
+        db_session.close()
+        return prenotazioni_posto
+
+    def search_prenotazioni_aula_by_utente(self, utente_id):
+        db_session = Session()
+        prenotazioni_aula = PrenotazioneAula().search_by_utente(utente_id)
+        db_session.close()
+        return prenotazioni_aula
+
+    def search_prenotazioni_posto_by_utente(self, utente_id):
+        db_session = Session()
+        prenotazioni_posto = PrenotazionePosto().search_by_utente(utente_id)
+        db_session.close()
+        return prenotazioni_posto
+
+    def __init__(self):
+        super().__init__()
--- view/home_utente.py
+++ view/home_utente.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QHBoxLayout
 from abstract.view import View
 from view.component import SidebarComponent
 from view.component.catalogo import CatalogoComponent
+from view.lisat_prenotazioni import ListaPrenotazioniView
 from view.scegli_prenotazione import ScegliPrenotazione
 
 
@@ -30,6 +31,7 @@ class HomeUtenteView(View):
         logout_button = self.get_button("Logout")
         logout_button.clicked.connect(self.send_logout_request)
         self.get_button("Prenota posto").clicked.connect(self.show_prenota_schermata)
+        self.get_button("Lista di prenotazioni").clicked.connect(self.show_lista_prenotazioni)
 
     def attach_controllers(self) -> None:
         from app import controller_logout
@@ -38,6 +40,10 @@ class HomeUtenteView(View):
     def show_prenota_schermata(self):
         scegli_prenotazione_view = ScegliPrenotazione()
         self.main_window.set_view(scegli_prenotazione_view)
+
+    def show_lista_prenotazioni(self):
+            lista_prenotazioni_view = ListaPrenotazioniView()
+            self.main_window.set_view(lista_prenotazioni_view)
 
     def __init__(self):
         super().__init__()
