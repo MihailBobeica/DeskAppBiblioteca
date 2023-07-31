05ba8754859cc28d2bd787a73e98c16a6e006e42
LoriGaetani <100944852+LoriGaetani@users.noreply.github.com>
2023-07-31 13:41:33 +0200

    prenotazione libro

--- model/prenotazione_libro.py
+++ model/prenotazione_libro.py
@@ -9,20 +9,20 @@ class PrenotazioneLibro(Model):
     def inserisci(self, libro : db_libro):
         db_session = Session()
         prenotazione_libro = db_prenotazione_libro(libro = libro.isbn, utente = Auth.user.username, data_prenotazione = datetime.utcnow(), data_scadenza = datetime.utcnow() + timedelta(days=3))
-        if self.sospensione():
+        '''if self.sospensione():
             pass
         if self.max_prenotazioni():
             pass
         if self.libro_non_disponibile():
             pass
-        else:
-            db_session.add(prenotazione_libro)
-            print(prenotazione_libro.utente)
-            print(prenotazione_libro.data_prenotazione)
-            print(prenotazione_libro.data_scadenza)
-            print(prenotazione_libro.libro)
-            db_session.commit()
-            db_session.close()
+        else:'''
+        db_session.add(prenotazione_libro)
+        print(prenotazione_libro.utente)
+        print(prenotazione_libro.data_prenotazione)
+        print(prenotazione_libro.data_scadenza)
+        print(prenotazione_libro.libro)
+        db_session.commit()
+        db_session.close()
 
 
     def __init__(self):
